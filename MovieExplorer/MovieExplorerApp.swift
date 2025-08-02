377bedc457b76569e34fb3f514df0ad5c4d39213
Roshan Karn <iroshankarn@gmail.com>
2025-08-02 13:46:01 +0530

    Initial Commit

---
+++ MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by Roshan Karn on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieExplorerTests/MovieExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerTests.swift
+//  MovieExplorerTests
+//
+//  Created by Roshan Karn on 02/08/25.
+//
+
+import Testing
+@testable import MovieExplorer
+
+struct MovieExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
